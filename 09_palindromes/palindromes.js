1ddb816e810b46f409e837f4c7d6895eb3850010
Jason Soprovich <190052169+jasonsoprovich@users.noreply.github.com>
2025-02-05 19:52:14 -0700

    feat: completed exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let cleanString = '';
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i].toLowerCase();
 
+    if ((char >= 'a' && char <= 'z') || (char >= '0' && char <= '9')) {
+      cleanString += char;
+    }
+  }
+
+  let reversedString = '';
+  for (let i = cleanString.length - 1; i>=0; i--) {
+    reversedString += cleanString[i];
+  }
+  return cleanString === reversedString;
 };
 
 // Do not edit below this line
